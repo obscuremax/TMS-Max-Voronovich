a780431f91bc12fc5a2c5141a8a0b75429b3cac5
obscuremax <maxxam@yandex.ru>
2019-12-15 16:41:57 +0300

    solve home work ( Upgrade you weather-app)

--- weather-app/index.js
+++ weather-app/index.js
@@ -7,11 +7,25 @@ const buttonFind = document.querySelector('#button');
 const tbody = document.querySelector('#tbody');
 const map = document.querySelector('#mapid');
 const body = document.querySelector("body");
+const myWeather = document.querySelector("#weather");
 const storage = new Map();
 
 let mymap;
 let marker;
 
+let keys = Object.keys(localStorage);
+for(let key of keys) {
+	fetch(`http://api.weatherstack.com/current?access_key=b397f298c1a0b7db79f668d3c47e27fb&query= ${key}`)
+		.then(result => result.json())
+		.then(data => {
+			const {
+				location: {name, country, lat, lon},
+				current: {temperature}
+			} = data
+			storage.set(name, [name, country, temperature]);
+			renderTbody()
+		})
+}
 
 function renderTbody() {
 	tbody.innerHTML = ''
@@ -21,10 +35,14 @@ function renderTbody() {
             <td>${city}</td>
             <td>${country}</td>
             <td>${temperature}</td>
-            <td>${temperature}</td>
+            <td>${temteratureInFarengeit(temperature)}</td>
         </tr>`
 		tbody.innerHTML = tbody.innerHTML + row
 	})
+}
+
+function temteratureInFarengeit(temperature) {
+	return ((temperature * 9/5) + 32)
 }
 
 buttonFind.addEventListener('click', function () {
@@ -35,8 +53,9 @@ buttonFind.addEventListener('click', function () {
 				location: {name, country, lat, lon},
 				current: {temperature}
 			} = data
-
+			let arr = [name, country, lat, lon, temperature]
 			storage.set(name, [name, country, temperature]);
+			localStorage.setItem(name, JSON.stringify(arr))
 
 			renderTbody();
 
@@ -56,9 +75,42 @@ buttonFind.addEventListener('click', function () {
 let buttonClear = document.getElementById('clear');
 buttonClear.addEventListener('click', function () {
 	storage.clear();
-	mymap.remove();
 	tbody.innerHTML = ' ';
 	map.innerHTML = '';
-	console.log(storage)
 })
+
+
+myWeather.addEventListener("click", function () {
+	let lat;
+	let lon;
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(position => {
+			lat = position.coords.latitude;
+			lon = position.coords.longitude;
+
+			fetch(`http://api.weatherstack.com/current?access_key=b397f298c1a0b7db79f668d3c47e27fb&query= ${lat},${lon}`)
+				.then(result => result.json())
+				.then(data =>{
+					const {
+						location: {region, country},
+						current: {temperature}
+					} = data
+
+					let arr = [region, country, lat, lon, temperature]
+					localStorage.setItem(region, JSON.stringify(arr))
+
+					storage.set(region, [region, country, temperature]);
+					renderTbody();
+				})
+
+		}, error =>{
+			console.log("error")
+		})
+	} else {
+			console.log("not supported")
+	}
+
+})
+
+
 
